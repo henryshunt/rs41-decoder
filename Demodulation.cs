3f34519ee7aa4da2a86dd03294f65dfc1be84bcb
Henry Hunt <henryhunt.dev@gmail.com>
2020-08-22 01:44:17 +0100

    Added method to read WAV file header

--- Demodulation.cs
+++ Demodulation.cs
@@ -1,16 +1,119 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace RS41Decoder
 {
     public class Demodulation
     {
+        public int NumberOfChannels { get; set; }
+        public int SampleRate { get; set; }
+        public int BitsPerSample { get; set; }
+        public double SamplesPerBit { get; set; }
+
+        private static int BAUD_RATE = 4800;
+
+        private bool hasReadWavHeader = false;
+
         private int par = 1;
         private int parOld = 1;
 
-        public void ReadWavHeader(BinaryReader reader)
-        {
 
+        public bool ReadWavHeader(BinaryReader reader)
+        {
+            byte[] buffer = new byte[4];
+
+            // Read RIFF header
+            if (reader.Read(buffer, 0, 4) < 4)
+                return false;
+
+            if (Encoding.UTF8.GetString(buffer) != "RIFF")
+                return false;
+
+            // Skip along
+            if (reader.Read(buffer, 0, 4) < 4)
+                return false;
+
+            // Read WAVE header
+            if (reader.Read(buffer, 0, 4) < 4)
+                return false;
+
+            if (Encoding.UTF8.GetString(buffer) != "WAVE")
+                return false;
+
+            // Read until "fmt " is found
+            string buffer2 = "";
+
+            while (true)
+            {
+                try
+                {
+                    buffer2 += reader.ReadChar();
+                }
+                catch (EndOfStreamException)
+                {
+                    return false;
+                }
+
+                if (buffer2.Contains("fmt "))
+                    break;
+            }
+
+            // Skip along
+            if (reader.Read(buffer, 0, 4) < 4)
+                return false;
+            if (reader.Read(buffer, 0, 2) < 2)
+                return false;
+
+            // Read channel count
+            if (reader.Read(buffer, 0, 2) < 2)
+                return false;
+
+            NumberOfChannels = buffer[0] + (buffer[1] << 8);
+
+            // Read sample rate
+            if (reader.Read(buffer, 0, 4) < 4)
+                return false;
+
+            SampleRate = BitConverter.ToInt32(buffer);
+            SamplesPerBit = SampleRate / (float)BAUD_RATE;
+
+            // Skip along
+            if (reader.Read(buffer, 0, 4) < 4)
+                return false;
+            if (reader.Read(buffer, 0, 2) < 2)
+                return false;
+
+            // Read bits per sample
+            if (reader.Read(buffer, 0, 2) < 2)
+                return false;
+
+            BitsPerSample = buffer[0] + (buffer[1] << 8);
+
+            if (BitsPerSample != 8 && BitsPerSample != 16)
+                return false;
+
+            // Read until "data" is found
+            while (true)
+            {
+                try
+                {
+                    buffer2 += reader.ReadChar();
+                }
+                catch (EndOfStreamException)
+                {
+                    return false;
+                }
+
+                if (buffer2.Contains("data"))
+                    break;
+            }
+
+            // Skip along
+            if (reader.Read(buffer, 0, 4) < 4)
+                return false;
+
+            return true;
         }
 
         public short ReadWavSample(BinaryReader reader)
--- RS41.cs
+++ RS41.cs
@@ -20,19 +20,7 @@ namespace RS41Decoder
             using (FileStream fileStream = File.OpenRead(input))
             {
                 using (BinaryReader reader = new BinaryReader(fileStream))
-                {
-                    for (int i = 0; i < 10; i++)
-                        reader.ReadByte();
-
-                    //for (int i = 0; i < 25; i++)
-                    //{
-                    //    var x = d.ReadBits(reader);
-                    //    Console.WriteLine("value: {0}, count: {1}", x.Item1, x.Item2);
-                    //    //Console.WriteLine(d.ReadWavSample(reader));
-                    //}
-
                     dc.DecodeLoop(reader, d);
-                }
             }
         }
     }
